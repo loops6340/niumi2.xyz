3887ced07ad3d97e395c3d6282cea325f3024e80
lops <gonzalesinciofabioleonel@gmail.com>
2026-07-30 01:52:32 -0500

    drag and drop de archivo para el input de mensaje...

--- src/lib/components/MessageSenderInput.svelte
+++ src/lib/components/MessageSenderInput.svelte
@@ -1,55 +1,105 @@
 <script lang="ts">
-	import { AiOutlineSend } from "svelte-icons-pack/ai";
-    import { BsFileEarmarkPlusFill } from "svelte-icons-pack/bs";
+	import { AiOutlineSend } from 'svelte-icons-pack/ai';
+	import { BsFileEarmarkPlusFill } from 'svelte-icons-pack/bs';
 
-    import { Icon } from 'svelte-icons-pack'
-	import DarkButton from "./UI/DarkButton.svelte";
-	import DarkInput from "./UI/DarkInput.svelte";
-	import { applyAction, enhance } from "$app/forms";
+	import { Icon } from 'svelte-icons-pack';
+	import DarkButton from './UI/DarkButton.svelte';
+	import DarkInput from './UI/DarkInput.svelte';
+	import { applyAction, enhance } from '$app/forms';
 
-    interface Props {
-        author: string;
-        disabled: boolean;
-        onsubmit: (user: {author: string, content: string}) => void
-        updateMessages: () => Promise<void>
-    }
+	interface Props {
+		author: string;
+		disabled: boolean;
+		onsubmit: (user: { author: string; content: string }) => void;
+		updateMessages: () => Promise<void>;
+	}
 
-    let {author, disabled, onsubmit, updateMessages}: Props = $props();
-    let selectedFile = $state<string>()
+	let { author, disabled, onsubmit, updateMessages }: Props = $props();
+	let selectedFile = $state<string>();
 
-    let textInput: HTMLInputElement;
-    let fileInput: HTMLInputElement;
+	let textInput: HTMLInputElement;
+	let fileInput: HTMLInputElement;
+
+	$effect(() => {
+		window.addEventListener('drop', (e) => {
+			if ([...e!.dataTransfer!.items].some((item) => item.kind === 'file')) {
+				e.preventDefault();
+                // console.log(fileInput.files)
+                fileInput.files = e!.dataTransfer!.files
+                console.log(fileInput.files)
+                selectedFile = fileInput.files[0].name
+			}
+		});
+
+		window.addEventListener('dragover', (e) => {
+			const fileItems = [...e!.dataTransfer!.items].filter((item) => item.kind === 'file');
+			if (fileItems.length > 0) {
+				e.preventDefault();
+			}
+		});
+	});
 </script>
-    
-    
-    <div class="mt-auto">
-        {#if selectedFile}
-            <div class="p-5 bg-dm-dark-primary text-dm-light-primary">
-                {selectedFile}
-            </div>
-        {/if}
 
-      <form method="POST" action="?/sendMessage" use:enhance={({ formData }) => {
-            const content = formData.get("content") as string
-            textInput.value = ""
-            fileInput.value = ""
-            selectedFile = ""
-            onsubmit({author, content})
-            return async ({result}) => {
-                await updateMessages()
-                applyAction(result)
-                textInput.focus()
-            }
-	    }} 
-        enctype="multipart/form-data" class="bg-white flex border border-black border-l-0">
-        <DarkInput disabled notRounded class="w-25" value={author} onchange={(e) => (author = e.currentTarget.value)} type="text" name="author" placeholder="autor" />
-        <label for="file-upload" class='cursor-pointer dark:bg-dm-light-primary  flex items-center p-2 border-l border-r'>
-            <Icon src={BsFileEarmarkPlusFill} />
-        </label>
-        <input bind:this={fileInput} onchange={(e) => selectedFile = (e.currentTarget.value)} id="file-upload" type="file" class="hidden" name="file" />
-        <input bind:this={textInput} spellCheck={false} disabled={disabled} type="text" name="content" class='p-2 dark:bg-dm-light-primary focus:outline-0 w-full'/>
-        <DarkButton notRounded class='w-20 flex items-center text-[20px]'>
-            <Icon src={AiOutlineSend} className="ml-auto mr-auto" />
-        </DarkButton>
-      </form>
-    </div>
+<div class="mt-auto">
+	{#if selectedFile}
+		<div class="bg-dm-dark-primary p-5 text-dm-light-primary">
+			{selectedFile}
+		</div>
+	{/if}
+
+	<form
+		method="POST"
+		action="?/sendMessage"
+		use:enhance={({ formData }) => {
+			const content = formData.get('content') as string;
+			textInput.value = '';
+			fileInput.value = '';
+			selectedFile = '';
+			onsubmit({ author, content });
+			return async ({ result }) => {
+				await updateMessages();
+				applyAction(result);
+				textInput.focus();
+			};
+		}}
+		enctype="multipart/form-data"
+		class="flex border border-l-0 border-black bg-white"
+	>
+		<DarkInput
+			disabled
+			notRounded
+			class="w-25"
+			value={author}
+			onchange={(e) => (author = e.currentTarget.value)}
+			type="text"
+			name="author"
+			placeholder="autor"
+		/>
+		<label
+			for="file-upload"
+			class="flex cursor-pointer items-center border-r border-l p-2 dark:bg-dm-light-primary"
+		>
+			<Icon src={BsFileEarmarkPlusFill} />
+		</label>
+		<input
+			bind:this={fileInput}
+			onchange={(e) => (selectedFile = e.currentTarget.value)}
+			id="file-upload"
+			type="file"
+			class="hidden"
+			name="file"
+		/>
+		<input
+			bind:this={textInput}
+			spellCheck={false}
+            placeholder="Arrastra algún archivo"
+			{disabled}
+			type="text"
+			name="content"
+			class="w-full p-2 focus:outline-0 dark:bg-dm-light-primary"
+		/>
+		<DarkButton notRounded class="flex w-20 items-center text-[20px]">
+			<Icon src={AiOutlineSend} className="ml-auto mr-auto" />
+		</DarkButton>
+	</form>
+</div>
